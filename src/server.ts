a3f8b1f2013b40333a01037620c4457f983fb836
issblann <anaisabelpp@outlook.com>
2025-05-05 01:10:34 -0500

    Feature: Add swagger yaml

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+import path from 'path';
 import app from './app';
 import dotenv from 'dotenv';
-
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
+app.use('/api-docs',  swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`API documentation is available at http://localhost:${PORT}/api-docs`);
 });
